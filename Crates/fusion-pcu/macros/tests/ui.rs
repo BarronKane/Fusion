79e3382cc9ae50cb6dfa5985cfb94f36bf11f2ec
Lance Wallis <walance@pvas.me>
2026-09-24 05:31:38 -0500

    Extract Fusion-PCU into standalone submodule

--- Crates/fusion-pcu/macros/tests/ui.rs
+++
@@ -1,7 +0,0 @@
-#[test]
-fn dispatch_macro_ui() {
-    let tests = trybuild::TestCases::new();
-    tests.compile_fail("tests/ui/unsupported_statement.rs");
-    tests.compile_fail("tests/ui/unsupported_expression.rs");
-    tests.pass("tests/ui/renamed_crate.rs");
-}
